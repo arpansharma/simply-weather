517d5275218cb30154db8a89ca47a769ac735315
arpansharma <arpan.sharma011@gmail.com>
2021-01-31 15:04:32 +0530

    Added function to fetch weather-info

--- index.js
+++ index.js
@@ -18,3 +18,37 @@ window.addEventListener('load', ()=> {
         });
     }
 });
+
+fetchWeatherInfo=(latitude, longitude, callBackFunc) => {
+    const cors_proxy = "https://cors-anywhere.herokuapp.com/";
+    const api_key = "YOUR_OPEN_WEATHER_API_KEY";
+    const api = `${cors_proxy}api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
+
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log("Weather API Response : ", data);
+            const {temp, temp_min, temp_max} = data.main
+            const {description} = data.weather[0]
+
+            // Set elements from the API
+            current_temp = (temp - 273.15).toFixed(2)
+            minimum_temp = (temp_min - 273.15).toFixed(2)
+            maximum_temp = (temp_max - 273.15).toFixed(2)
+            weather_description = description
+
+            response = {
+                "latitude": latitude,
+                "longitude": longitude,
+                "current_temp": current_temp,
+                "minimum_temp": minimum_temp,
+                "maximum_temp": maximum_temp,
+                "weather_description": weather_description,
+            }
+            console.log("fetchWeatherInfo Response : ", response);
+
+            callBackFunc(response)
+        });
+}
